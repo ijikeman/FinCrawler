a41ef32c8b0ff5df7c7d78de05bdb3de782ebb26
ijikeman <keiji.ue@gmail.com>
2017-05-02 00:34:54 +0900

    fixed namespace

--- lib/parser/yahoo.rb
+++ lib/parser/yahoo.rb
@@ -1,21 +1,17 @@
-module Lib
-  module Parser
-    class Yahoo < Lib::Parser
-      BASE_URL = 'https://stocks.finance.yahoo.co.jp/'
+module Parser
+  class Yahoo < Parser
+    BASE_URL = 'https://stocks.finance.yahoo.co.jp/'
+    def parse
+      doc = open
+      current_price = doc.xpath("//td[@class='stoksPrice']").text
+      before_ratio = doc.xpath("//span[@class='icoDownRed yjMSt']").text
+      p current_price
+      p before_ratio
+    end
 
-      def parse
-        doc = open
-        current_price = doc.xpath("//td[@class='stoksPrice']").text
-        before_ratio = doc.xpath("//span[@class='icoDownRed yjMSt']").text
-        p current_price
-        p before_ratio
-      end
-
-      private
-      def url
-        "#{BASE_URL}stocks/detail/?code=#{@code}"
-      end
-
+    private
+    def url
+      "#{BASE_URL}stocks/detail/?code=#{@code}"
     end
   end
 end
