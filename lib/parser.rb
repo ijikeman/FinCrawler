0041ac8339e259ca192830700d49555a1a6d2099
ijikeman <keiji.ue@gmail.com>
2017-05-02 00:51:38 +0900

    fixed namespace

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,23 +1,21 @@
-module Lib
-  require 'open-uri'
-  require 'nokogiri'
+require 'open-uri'
+require 'nokogiri'
 
-  class Parser
-    def initialize(code)
-      @code = code
-    end
+class Parser
+  def initialize(code)
+    @code = code
+  end
 
-    def parse
-      doc = nokogiri_open
-    end
+  def parse
+    doc = nokogiri_open
+  end
 
-    private
-    def url
-      'http://'
-    end
+  private
+  def url
+    'http://'
+  end
 
-    def nokogiri_open
-      Nokogiri::HTML(open(url))
-    end
+  def nokogiri_open
+    Nokogiri::HTML(open(url))
   end
 end
--- lib/parser/yahoo.rb
+++ lib/parser/yahoo.rb
@@ -1,19 +1,17 @@
-module Lib
-  module Parser
-    class Yahoo < Lib::Parser
-      BASE_URL = 'https://stocks.finance.yahoo.co.jp/'
-      def parse
-        doc = nokogiri_open
-        current_price = doc.xpath("//td[@class='stoksPrice']").text
-        before_ratio = doc.xpath("//span[@class='icoDownRed yjMSt']").text
-        p current_price
-        p before_ratio
-      end
+module Parser
+  class Yahoo < Parser
+    BASE_URL = 'https://stocks.finance.yahoo.co.jp/'
+    def parse
+      doc = nokogiri_open
+      current_price = doc.xpath("//td[@class='stoksPrice']").text
+      before_ratio = doc.xpath("//span[@class='icoDownRed yjMSt']").text
+      p current_price
+      p before_ratio
+    end
 
-      private
-      def url
-        "#{BASE_URL}stocks/detail/?code=#{@code}"
-      end
+    private
+    def url
+      "#{BASE_URL}stocks/detail/?code=#{@code}"
     end
   end
 end
